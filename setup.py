069c24ab43033f7a5b3e55d7aea306b41b736d29
Gopakumar Mohandas <17816393+gmohandas@users.noreply.github.com>
2020-03-20 23:17:13 +0100

    minimal install requirements

--- setup.py
+++ setup.py
@@ -50,5 +50,5 @@ setuptools.setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=install_requires
+    install_requires=['pandas', 'requests']
 )
