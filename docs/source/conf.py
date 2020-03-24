6d4faa65d5913a4707c7401c0b94bf39b2fc24b9
Gopakumar Mohandas <17816393+gmohandas@users.noreply.github.com>
2020-03-24 21:31:03 +0100

    set master doc to index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,9 @@ templates_path = ['_templates']
 #
 source_suffix = ['.rst', '.md']
 
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
